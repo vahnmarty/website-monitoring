c8232b232a6e5626583af0cec894a7e88df53165
Vahn <vahnmarty@gmail.com>
2019-03-06 17:39:41 +0800

    notification

--- js/custom.js
+++ js/custom.js
@@ -6,47 +6,7 @@ $(document).ready(function(){
     console.log(time);
 });
 
-$(document).ready(function(){
-    var table = $('#table-web tbody');
-    var ping = new Date;
-    $.ajax({
-        url: 'websites.json',
-        success:function(data){
-            $.each(data,function(i, item){
-                console.log('Pinging ' + item['domain']);
-                var time = new Date;
-                $.ajax({
-                    url: item['domain'],
-                    cache:false,
-                    success:function(data, textStatus, xhr){
-                        ping = new Date - time;
 
-                        var tr = $('<tr>');
-                        tr.append('<td><i class="fa fa-circle text-orange" data-toggle="tooltip" data-placement="top" title="In Progress"></i></td>');
-                        tr.append('<td>'+item["category"]+'</td>');
-                        tr.append('<td>'+item["website"]+'</td>');
-                        tr.append('<td>'+item["domain"]+'</td>');
-                        tr.append('<td>'+ping+' ms</td>');
-                        tr.append('<td>'+xhr.status+'</td>');
-                        table.append(tr);
-                    },
-                    error:function(data, textStatus, xhr){
-                        var tr = $('<tr>');
-                        tr.append('<td><i class="fa fa-circle text-orange" data-toggle="tooltip" data-placement="top" title="In Progress"></i></td>');
-                        tr.append('<td>'+item["category"]+'</td>');
-                        tr.append('<td>'+item["website"]+'</td>');
-                        tr.append('<td>'+item["domain"]+'</td>');
-                        tr.append('<td>-</td>');
-                        tr.append('<td>'+xhr.status+'</td>');
-                        table.append(tr);
-                    }
-                });
-
-                
-            });
-        }
-    });
-});
 
 $('#form-ping').on('submit', function(e){
     e.preventDefault();
@@ -65,3 +25,96 @@ $('#form-ping').on('submit', function(e){
         }
     });
 });
+
+monitorNow();
+refreshTimer();
+
+function refreshTimer(){
+    $(document).ready(function(){
+        var timeleft = 60;
+        var downloadTimer = 
+        setInterval(function(){
+            timeleft--;
+            document.getElementById("countdowntimer").textContent = timeleft;
+            if(timeleft <= 0){
+                clearInterval(downloadTimer);
+                refreshTimer();
+                monitorNow();
+            }
+        },1000);
+    });
+}
+
+function monitorNow(){
+    $(document).ready(function(){
+        var table = $('#table-web tbody');
+        table.empty();
+        var ping = new Date;
+        $.ajax({
+            url: 'websites.json',
+            success:function(data){
+                $.each(data,function(i, item){
+                    console.log('Pinging ' + item['domain']);
+                    var time = new Date;
+                    $.ajax({
+                        url: item['domain'],
+                        cache:false,
+                        success:function(data, textStatus, xhr){
+                            ping = new Date - time;
+
+                            var tr = $('<tr>');
+                            tr.append('<td><i class="fa fa-circle text-success" data-toggle="tooltip" data-placement="top" title="OK"></i></td>');
+                            tr.append('<td>'+item["category"]+'</td>');
+                            tr.append('<td>'+item["website"]+'</td>');
+                            tr.append('<td>'+item["domain"]+'</td>');
+                            tr.append('<td>'+ping+' ms</td>');
+                            tr.append('<td>'+xhr.status+'</td>');
+                            table.append(tr);
+                        },
+                        error:function(data, textStatus, xhr){
+                            var tr = $('<tr>');
+                            tr.append('<td><i class="fa fa-circle text-danger" data-toggle="tooltip" data-placement="top" title="ERROR"></i></td>');
+                            tr.append('<td>'+item["category"]+'</td>');
+                            tr.append('<td>'+item["website"]+'</td>');
+                            tr.append('<td>'+item["domain"]+'</td>');
+                            tr.append('<td>-</td>');
+                            tr.append('<td>'+xhr.status+'</td>');
+                            table.append(tr);
+                            notifyMe(item["website"], item["domain"]);
+                        }
+                    });
+
+                    
+                });
+            }
+        });
+    });
+}
+
+// request permission on page load
+document.addEventListener('DOMContentLoaded', function () {
+  if (!Notification) {
+    alert('Desktop notifications not available in your browser. Try Chromium.'); 
+    return;
+  }
+
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
+});
+
+function notifyMe(site, url) {
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
+  else {
+    var notification = new Notification('Server Alert', {
+      icon: '../img/logo-light.png',
+      body: site + ' is down!',
+    });
+
+    notification.onclick = function () {
+      window.open(url);      
+    };
+
+  }
+
+}
